c1c26675de5c280ea5c682be2bad228b86c14e63
oipocorp <procopio@gmail.com>
2021-11-07 22:15:58 -0300

    pequenas melhorias

--- levlog.py
+++ levlog.py
@@ -3,21 +3,19 @@ from pprint import pprint
 import Levenshtein
 
 lev_treshold = float(sys.argv[1])
-input_file_names = sys.argv[2:]
-
+input_filenames = sys.argv[2:]
 log_dict = {}
 
-for input_file_name in input_file_names:
-    with open(input_file_name) as log_file:
-        for log_line in log_file.readlines():
+for input_filename in input_filenames:
+    with open(input_filename) as log_file:
+        for log_line in [line.rstrip('\n') for line in log_file]:
 
-            log_line = log_line.strip('\n')
             matched = False
-
             for saved_log_line in log_dict.keys():
                 if Levenshtein.ratio(saved_log_line, log_line) >= lev_treshold:
                     log_dict[saved_log_line] += 1
                     matched = True
+                    break
 
             if not matched:
                 log_dict[log_line] = 1
